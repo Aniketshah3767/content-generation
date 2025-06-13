b311ebf8de295305b1c4f9f6d90ee0da745acdf6
Aniketshah3767 <79820398+Aniketshah3767@users.noreply.github.com>
2025-06-13 17:21:14 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images:{
-    domains: ['cdn-icons-png.flaticon.com']
-  }
+  images: {
+    domains: ['cdn-icons-png.flaticon.com'],
+  },
+  eslint: {
+    // This skips ESLint during production builds on Vercel (optional for now)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
